188a64e16709671ef40b2297fa8905468d9b9576
sum-kcid <jayp1924@gmail.com>
2016-01-26 15:15:45 -0500

    inited html and other public repo files - styling and client-side scripts is next

--- app/client/scripts/app.js
+++ app/client/scripts/app.js
@@ -1,13 +1,3 @@
 if (Meteor.isClient) {
-	Session.setDefault('counter', 0);
-	Template.hello.helpers({
-		counter: function () {
-			return Session.get('counter');
-		}
-	});
-	Template.hello.events({
-		'click button': function () {
-			Session.set('counter', Session.get('counter')+1);
-		}
-	});
+	
 };
